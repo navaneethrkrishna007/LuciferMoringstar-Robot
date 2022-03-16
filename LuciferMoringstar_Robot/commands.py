2cb03846006bea101207431a7b1e3be6abbb7e3f
Navaneeth R Krishna <98873149+navaneethrkrishna007@users.noreply.github.com>
2022-03-16 12:36:19 +0530

    Update commands.py

--- LuciferMoringstar_Robot/commands.py
+++ LuciferMoringstar_Robot/commands.py
@@ -20,8 +20,8 @@ async def start_message(bot, message):
              InlineKeyboardButton("ℹ️ Help", callback_data="help"),
              InlineKeyboardButton("😎 About", callback_data="about") 
              ],[
-             InlineKeyboardButton("🗳 Deploy", url="https://youtu.be/OTqZmADyOjU"),
-             InlineKeyboardButton("🤖 Support", url="https://t.me/Mo_Tech_YT")
+             InlineKeyboardButton("🗳 Deploy", url="https://github.com/navaneethrkrishna007/LuciferMoringstar-Robot"),
+             InlineKeyboardButton("🤖 Support", url="https://t.me/cinemavilla_updates")
              ]]
         else:
             buttons = [[
@@ -30,8 +30,8 @@ async def start_message(bot, message):
              InlineKeyboardButton("ℹ️ Help", callback_data="bot_owner"),
              InlineKeyboardButton("😎 About", callback_data="about") 
              ],[
-             InlineKeyboardButton("🗳 Deploy", url="https://youtu.be/OTqZmADyOjU"),
-             InlineKeyboardButton("🤖 Support", url="https://t.me/Mo_Tech_Group")
+             InlineKeyboardButton("🗳 Deploy", url="https://github.com/navaneethrkrishna007/LuciferMoringstar-Robot"),
+             InlineKeyboardButton("🤖 Support", url="https://t.me/cinemavilla_updates")
              ]]    
         await message.reply_photo(photo = choice(BOT_PICS), caption=START_MSG.format(mention = message.from_user.mention, bot_name = bot_info.BOT_NAME, bot_username = bot_info.BOT_USERNAME), reply_markup=InlineKeyboardMarkup(buttons))
         
